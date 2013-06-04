0f9916bcde6387a6ba1756a82a1177cfc9da8356
S516490 <S516490@NW21597.nwmissouri.edu>
2013-06-04 11:27:31 -0500

    Project Added

---
+++ src/ragi/Ragi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ragi;
+
+/**
+ *
+ * @author S516490
+ */
+public class Ragi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
